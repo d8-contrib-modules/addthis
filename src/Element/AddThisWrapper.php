3ed848e57a3d590f44d6ec875070b256a1f258d0
Doyle <doylejd88@gmail.com>
2015-08-18 14:45:26 -0400

    Removed deprecated code

--- src/Element/AddThisWrapper.php
+++ src/Element/AddThisWrapper.php
@@ -24,26 +24,8 @@ class AddThisWrapper extends RenderElement {
     $class = get_class($this);
     return array(
       '#theme' => 'addthis_wrapper',
-      '#pre_render' => [
-        [$class, 'preRender'],
-      ],
     );
   }
 
-
-  public function preRender($element){
-    //$element['#attributes'] = new Attribute($element['#attributes']);
-//    $children = Element::children($element);
-//    $element['services'] = [];
-//    if (count($children) > 0) {
-//      foreach ($children as $child) {
-//        //@TODO Figure out why we need to call render() here and it isn't handled
-//        //by printing {{element}} in the twig template.
-//        $element['services'][] = render($element[$child]);
-//      }
-//    }
-
-    return $element;
-  }
 
 }
