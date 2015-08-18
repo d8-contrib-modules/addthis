d718e0571f6f9ee558a72703b4301a1231c59a25
Doyle <doylejd88@gmail.com>
2015-08-18 11:56:33 -0400

    Updated AddThisBasicToolbox to render proper markup.

--- src/Element/AddThisElement.php
+++ src/Element/AddThisElement.php
@@ -20,9 +20,27 @@ class AddThisElement extends RenderElement {
    * {@inheritdoc}
    */
   public function getInfo() {
+    $class = get_class($this);
     return [
       '#theme' => 'addthis_element',
+      '#attributes' => array(),
+      '#pre_render' => [
+        [$class, 'preRender'],
+      ],
     ];
+  }
+
+  /**
+   * PreRender function for addthis_element
+   *
+   * @TODO: Figure out why twig doesn't handle #attributes OOTB.
+   *
+   * @param $element
+   * @return mixed
+   */
+  public function preRender($element){
+    $element['#attributes'] = new Attribute($element['#attributes']);
+    return $element;
   }
 
 }
