ca8db7bde8aab709af278d201ff090cb2c74b8c5
Jason Enter <jason.enter@acquia.com>
2015-08-26 14:55:00 -0400

    move cleanup based on old elements

--- src/AddThis.php
+++ src/AddThis.php
@@ -14,55 +14,6 @@ use Drupal\Component\Utility\SafeMarkup;
 
 class AddThis {
 
-  // Persistent variable keys.
-  /*
-  const SERVICES_CSS_URL_KEY = 'addthis_services_css_url';
-    const SERVICES_JSON_URL_KEY = 'addthis_services_json_url';
-  */
-
-  // External resources.
-  const DEFAULT_BOOKMARK_URL = 'http://www.addthis.com/bookmark.php?v=300';
-  const DEFAULT_SERVICES_CSS_URL = 'http://cache.addthiscdn.com/icons/v1/sprites/services.css';
-  const DEFAULT_SERVICES_JSON_URL = 'http://cache.addthiscdn.com/services/v1/sharing.en.json';
-  const DEFAULT_WIDGET_JS_URL = 'http://s7.addthis.com/js/300/addthis_widget.js';
-  const DEFAULT_WIDGET_JS_LOAD_DOMREADY = TRUE;
-  const DEFAULT_WIDGET_JS_LOAD_ASYNC = FALSE;
-
-  // Type of inclusion.
-  // 0 = don't include, 1 = pages no admin, 2 = on usages only.
-  const DEFAULT_WIDGET_JS_INCLUDE = 2;
-  const WIDGET_JS_INCLUDE_NONE = 0;
-  const WIDGET_JS_INCLUDE_PAGE = 1;
-  const WIDGET_JS_INCLUDE_USAGE = 2;
-
-
-  // Internal resources.
-  const ADMIN_CSS_FILE = 'addthis.admin.css';
-  const ADMIN_INCLUDE_FILE = 'includes/addthis.admin.inc';
-
-  // Widget types.
-  const WIDGET_TYPE_DISABLED = 'addthis_disabled';
-
-  // Styles.
-  const CSS_32x32 = 'addthis_32x32_style';
-  const CSS_16x16 = 'addthis_16x16_style';
-  private static $instance;
-
-  /* @var AddThisJson */
-  private $json;
-
-  private $config;
-
-  /**
-   * @var \Drupal\addthis\AddThisScriptManager.
-   */
-  protected $add_this_script_manager;
-
-  /**
-   * @var \Drupal\Core\Config\ConfigFactory
-   */
-  protected $config_factory;
-
   /**
    * @param \Drupal\addthis\AddThisScriptManager $addThisScriptManager
    */
@@ -72,6 +23,11 @@ class AddThis {
     $this->json = new AddThisJson();
     $this->setConfig();
   }
+
+
+  /* @var AddThisJson */
+  private $json;
+  private $config;
 
   /**
    * Set the json object.
@@ -109,6 +65,7 @@ class AddThis {
     return $rows;
   }
 
+  const DEFAULT_SERVICES_JSON_URL = 'http://cache.addthiscdn.com/services/v1/sharing.en.json';
 
   public function getServicesJsonUrl() {
     $service_json_url_key = $this->config->get(self::SERVICES_JSON_URL_KEY);
