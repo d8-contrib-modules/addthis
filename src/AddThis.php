3731e7df4c86d9e9dbf07ac115e745adbd31ef67
Jason Enter <jason.enter@acquia.com>
2015-08-26 14:47:02 -0400

    first draft of removing constants and old functions

--- src/AddThis.php
+++ src/AddThis.php
@@ -13,36 +13,12 @@ use Drupal\addthis\Util\AddThisJson;
 use Drupal\Component\Utility\SafeMarkup;
 
 class AddThis {
-  const BLOCK_NAME = 'addthis_block';
-  const DEFAULT_CUSTOM_CONFIGURATION_CODE = 'var addthis_config = {}';
-  const DEFAULT_FORMATTER = 'addthis_default_formatter';
-  const DEFAULT_NUMBER_OF_PREFERRED_SERVICES = 4;
-  const FIELD_TYPE = 'addthis';
-  const MODULE_NAME = 'addthis';
-  const PERMISSION_ADMINISTER_ADDTHIS = 'administer addthis';
-  const PERMISSION_ADMINISTER_ADVANCED_ADDTHIS = 'administer advanced addthis';
-  const STYLE_KEY = 'addthis_style';
-  const WIDGET_TYPE = 'addthis_button_widget';
-
-  // AddThis attribute and parameter names (as defined in AddThis APIs).
-  const PROFILE_ID_QUERY_PARAMETER = 'pubid';
-  const TITLE_ATTRIBUTE = 'addthis:title';
-  const URL_ATTRIBUTE = 'addthis:url';
-
 
   // Persistent variable keys.
-  const BLOCK_WIDGET_TYPE_KEY = 'addthis_block_widget_type';
-  const BLOCK_WIDGET_SETTINGS_KEY = 'addthis_block_widget_settings';
-  const BOOKMARK_URL_KEY = 'addthis_bookmark_url';
-  const CUSTOM_CONFIGURATION_CODE_ENABLED_KEY = 'addthis_custom_configuration_code_enabled';
-  const CUSTOM_CONFIGURATION_CODE_KEY = 'addthis_custom_configuration_code';
+  /*
   const SERVICES_CSS_URL_KEY = 'addthis_services_css_url';
-  const SERVICES_JSON_URL_KEY = 'addthis_services_json_url';
-
-  const WIDGET_JS_URL_KEY = 'addthis_widget_js_url';
-  const WIDGET_JS_LOAD_DOMREADY = 'addthis_widget_load_domready';
-  const WIDGET_JS_LOAD_ASYNC = 'addthis_widget_load_async';
-  const WIDGET_JS_INCLUDE = 'addthis_widget_include';
+    const SERVICES_JSON_URL_KEY = 'addthis_services_json_url';
+  */
 
   // External resources.
   const DEFAULT_BOOKMARK_URL = 'http://www.addthis.com/bookmark.php?v=300';
@@ -90,7 +66,7 @@ class AddThis {
   /**
    * @param \Drupal\addthis\AddThisScriptManager $addThisScriptManager
    */
-  public function __construct(\Drupal\addthis\AddThisScriptManager $addThisScriptManager, \Drupal\Core\Config\ConfigFactory $configFactory){
+  public function __construct(\Drupal\addthis\AddThisScriptManager $addThisScriptManager, \Drupal\Core\Config\ConfigFactory $configFactory) {
     $this->add_this_script_manager = $addThisScriptManager;
     $this->config_factory = $configFactory;
     $this->json = new AddThisJson();
@@ -134,117 +110,11 @@ class AddThis {
   }
 
 
-  /**
-   * Get the settings used by the block display.
-   */
-  public function getBlockDisplaySettings($widget_type) {
-    $block_widget_settings = $this->config->get(self::BLOCK_WIDGET_SETTINGS_KEY);
-    $block_widget_settings = isset($block_widget_settings) ? $block_widget_settings : NULL;
-    $settings = $block_widget_settings;
-
-    if ($settings == NULL && $widget_type != self::WIDGET_TYPE_DISABLED) {
-      $settings = \Drupal::service('plugin.manager.field.formatter')
-        ->getDefaultSettings($widget_type);
-
-    }
-
-    return $settings;
-  }
-
-
-  public function getServicesCssUrl() {
-    $services_css_url_key = $this->config->get(self::SERVICES_CSS_URL_KEY);
-    $services_css_url_key = isset($services_css_url_key) ? $services_css_url_key : self::DEFAULT_SERVICES_CSS_URL;
-    return check_url($services_css_url_key);
-  }
-
   public function getServicesJsonUrl() {
     $service_json_url_key = $this->config->get(self::SERVICES_JSON_URL_KEY);
     $service_json_url_key = isset($service_json_url_key) ? $service_json_url_key : self::DEFAULT_SERVICES_JSON_URL;
     return check_url($service_json_url_key);
   }
-
-
-  /**
-   * Return the type of inclusion.
-   *
-   * @return string
-   *   Retuns domready or async.
-   */
-  public function getWidgetJsInclude() {
-    $widget_js_include = $this->config->get(self::WIDGET_JS_INCLUDE);
-    $widget_js_include = isset($widget_js_include) ? $widget_js_include : self::DEFAULT_WIDGET_JS_INCLUDE;
-    return $widget_js_include;
-  }
-
-  /**
-   * Return if domready loading should be active.
-   *
-   * @return bool
-   *   Returns TRUE if domready is enabled.
-   */
-  public function getWidgetJsDomReady() {
-    $widget_js_load_domready = $this->config->get(self::WIDGET_JS_LOAD_DOMREADY);
-    $widget_js_load_domready = isset($widget_js_load_domready) ? $widget_js_load_domready : self::DEFAULT_WIDGET_JS_LOAD_DOMREADY;
-    return $widget_js_load_domready;
-  }
-
-  /**
-   * Return if async initialization should be active.
-   *
-   * @return bool
-   *   Returns TRUE if async is enabled.
-   */
-  public function getWidgetJsAsync() {
-    $widget_js_load_async = $this->config->get(self::WIDGET_JS_LOAD_ASYNC);
-    $widget_js_load_async = isset($widget_js_load_async) ? $widget_js_load_async : self::DEFAULT_WIDGET_JS_LOAD_ASYNC;
-    return $widget_js_load_async;
-  }
-
-
-  public function getCustomConfigurationCode() {
-    $custom_configuration_code = $this->config->get(self::CUSTOM_CONFIGURATION_CODE_KEY);
-    $custom_configuration_code = isset($custom_configuration_code) ? $custom_configuration_code : self::DEFAULT_CUSTOM_CONFIGURATION_CODE;
-    return $custom_configuration_code;
-  }
-
-  public function isCustomConfigurationCodeEnabled() {
-    $custom_configuration_code_enabled = $this->config->get(self::CUSTOM_CONFIGURATION_CODE_ENABLED_KEY);
-    $custom_configuration_code_enabled = isset($custom_configuration_code_enabled) ? $custom_configuration_code_enabled : FALSE;
-
-    return (boolean) $custom_configuration_code_enabled;
-  }
-
-  public function getBaseWidgetJsUrl() {
-    $widget_js_url = $this->config->get(self::WIDGET_JS_URL_KEY);
-    $widget_js_url = isset($widget_js_url) ? $widget_js_url : self::DEFAULT_WIDGET_JS_URL;
-    return check_url($widget_js_url);
-  }
-
-  public function getBaseBookmarkUrl() {
-    $bookmark_url = $this->config->get(self::BOOKMARK_URL_KEY);
-    $bookmark_url = isset($bookmark_url) ? $bookmark_url : self::DEFAULT_BOOKMARK_URL;
-    return check_url($bookmark_url);
-  }
-
-
-  public function getFullBookmarkUrl() {
-    return $this->getBaseBookmarkUrl() . $this->getProfileIdQueryParameterPrefixedWithAmp();
-  }
-
-  private function getProfileIdQueryParameter($prefix) {
-    $profileId = $this->getProfileId();
-    return !empty($profileId) ? $prefix . self::PROFILE_ID_QUERY_PARAMETER . '=' . $profileId : '';
-  }
-
-  private function getProfileIdQueryParameterPrefixedWithAmp() {
-    return $this->getProfileIdQueryParameter('&');
-  }
-
-  private function getProfileIdQueryParameterPrefixedWithHash() {
-    return $this->getProfileIdQueryParameter('#');
-  }
-
 
 
 }
