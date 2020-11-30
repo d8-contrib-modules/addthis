ce0c8b9d6db44cdd2898b9eef1f9481a13ce09fb
Ron Ferguson <r0nn1ef8580@gmail.com>
2020-11-30 15:35:19 -0600

    added correct implementation of function

--- src/Plugin/Field/FieldWidget/AddThisWidget.php
+++ src/Plugin/Field/FieldWidget/AddThisWidget.php
@@ -52,6 +52,13 @@ class AddThisWidget extends WidgetBase {
    * {@inheritdoc}
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $element += [
+      '#type' => 'textfield',
+      '#default_value' => '',
+      '#size' => 7,
+      '#maxlength' => 7,
+    ];
+
     return $element;
   }
 
